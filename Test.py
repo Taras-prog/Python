4b4de962e8134aafae90f0450cbe7d0ed40f0c20
Taras-prog <tarascr7@gmail.com>
2020-07-12 16:16:59 +0300

    some commit

--- Test.py
+++ Test.py
@@ -1,2 +1,60 @@
-print('Hello world!')
-print('this is test')
+# # int
+# number = 5
+
+# # float
+# fnumber = 5.8
+
+# # str
+# name = "Taras"
+# age = 25
+
+# # bool
+
+# status = True
+
+# # Экранирование
+# print("This \"is\" SOME TEXT")
+
+# #Перевод строки
+# print('Hello \n world')
+
+# #Конкатенация 
+# print("Hi my name is " + name )
+# print("I am " + str(age))
+
+# name = input('Write your name: ')
+# age = input("Write your age: ")
+
+# print("Hello " + name + "!")
+# print("You are " + age + " this is cool!")
+
+# -------------------Arithmetic operators--------------------
+
+# a = 5
+# b = 10
+
+# c = a + b
+# c = b - a
+# c = a * a
+# c = b / a
+# c = a ** 2 # возведение в степень
+# c = a % 2 # деление по модулю (возвращает остаток)
+# print(c)
+
+# unary minus
+# a = 10
+ 
+# a = -a 
+# a = -a
+
+# print(a)
+
+# rounding
+
+# import math
+
+# a = 7.345
+
+# print(math.ceil(a))
+
+# print(math.pi)
