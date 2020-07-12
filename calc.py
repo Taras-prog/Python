4f4bdbb3bda9494db656652e892bb4541e41e6d8
Taras-prog <tarascr7@gmail.com>
2020-07-12 17:17:19 +0300

    calculator

---
+++ calc.py
@@ -0,0 +1,26 @@
+# Simple calculator v1
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+print(Back.RED)
+print(Fore.BLACK)
+
+what = input("What are we doing? (+ or -)")
+
+print(Back.YELLOW)
+
+a = float( input("Enter the first number: "))
+b =  float ( input("Enter the second number: "))
+
+print(Back.MAGENTA)
+if what == "+":
+    c = a + b
+    print("Result: " + str(c))
+
+elif  what == "-":
+    c = a - b
+    print("Result: " + str(c))
+else:
+    print("Wrong operation selected")
+
+input()
